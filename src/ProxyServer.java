ae77e6b9b1b0f28bf8b8ef120910e27236a06e97
clairecassidy <clairecassidy0@hotmail.com>
2021-03-08 18:52:18 +0000

    can receive connection request from browser

--- src/ProxyServer.java
+++ src/ProxyServer.java
@@ -1,6 +1,30 @@
+import com.sun.security.ntlm.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ProxyServer implements Runnable {
 
     private boolean stopped = false;
+
+    public ProxyServer(int port) {
+
+        // set up socket on specified port
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server established on port "+port);
+
+            // wait for connection
+            Socket incoming = serverSocket.accept();
+            System.out.println("Client info: "+incoming.toString());
+
+        } catch (IOException e) {
+            System.out.println("Error creating socket");
+            e.printStackTrace();
+        }
+
+    }
 
     // may be called from Launcher to stop the server
     public synchronized void stop() {
@@ -16,11 +40,11 @@ public class ProxyServer implements Runnable {
     @Override
     public void run() {
         System.out.println("ProxyServer Running");
-
-        int count = 0;
-        while (!this.stopped) {
-            count++;
-            System.out.println(count);
-        }
+//
+//        int count = 0;
+//        while (!this.stopped) {
+//            count++;
+//            System.out.println(count);
+//        }
     }
 }
