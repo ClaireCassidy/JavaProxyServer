1ffdda24e6ed1a591c20320a2bc6394e91ef64c7
clairecassidy <clairecassidy0@hotmail.com>
2021-03-10 19:18:49 +0000

    add dynamic blocked sites data structure and getter

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 // Creates a ProxyServer and runs it
 public class Launcher {
     public static void main(String[] args) throws IOException {
+
         ProxyServer proxy = new ProxyServer(8080);
+
+        Thread mgmtConsole = new Thread(new ManagementConsole());
+        mgmtConsole.start();
 
         Thread proxyThread = new Thread(proxy);
         proxyThread.start();
 
-        Thread mgmtConsole = new Thread(new ManagementConsole());
-        mgmtConsole.start();
+
 
 //        try {
 //            proxyThread.sleep(2000);
--- src/ManagementConsole.java
+++ src/ManagementConsole.java
@@ -1,8 +1,45 @@
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ManagementConsole implements Runnable {
 
     private boolean stopped = false;
+    private static ArrayList<String> blockedSitesInstance = null;
+
+
+    public ManagementConsole() {
+        // open blocked sites file
+
+        File blockedSitesFile = new File("res\\blocked.txt");
+
+        try {
+            if (blockedSitesFile.exists()) {
+                Scanner sc = new Scanner(blockedSitesFile);     //file to be scanned
+                blockedSitesInstance = new ArrayList<>();
+
+                System.out.println("Blocked:");
+
+                String site;
+                while (sc.hasNextLine()) {
+                    site = sc.nextLine();
+                    System.out.println(site);
+                    blockedSitesInstance.add(site); // update dynamic blocked sites
+                }
+            } else {
+                System.out.println(this.toString()+"Couldn't find blocked sites file - check path");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(this.toString()+"Failed to open blocked sites file");
+
+            e.printStackTrace();
+        }
+
+    }
+
+    public static ArrayList<String> getBlockedSites() {
+        return blockedSitesInstance;
+    }
 
     // may be called from Launcher to stop the mgmt console
     public synchronized void stop() {
@@ -26,4 +63,10 @@ public class ManagementConsole implements Runnable {
 
         in.close();
     }
+
+    @Override
+    public String toString() {
+        return "\tMGMT-CONSOLE: ";
+    }
 }
+
